5876239fa7b6759ff3698fe37effc7e5af2db464
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-14 14:09:51 -0300

    ajustando bug  de home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import { globalStyles } from '@/styles/global'
-import { Inter } from 'next/font/google'
+import { Inter ,Roboto} from 'next/font/google'
 globalStyles()
 const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '100'
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 import { AppProps } from "next/app";
 globalStyles();
 
 export default function App({Component, pageProps}: AppProps){
-  return <Component {...pageProps}/>
+  return (
+  <Container>
+    <Header>
+      <h2>Logo</h2>
+    </Header>
+  <Component {...pageProps}/>
+  </Container>
+  )
 }
