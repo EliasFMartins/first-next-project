4c3c1e8ded151482db45a44f43b725b54243b2b6
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-15 18:24:31 -0300

    fechand page prod

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,19 +1,56 @@
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/products"
 import { useRouter } from "next/router"
+import { GetStaticProps } from "next";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+import Image from 'next/image';
 
-export default function Product() {
-  const { query} = useRouter()
+interface ProductProps {
+  product:{
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string
+  }[];
+}
+
+export default function Product({product}: ProductProps ) {
   return(
  <ProductContainer>
   <ImageContainer>
-
+    <Image src={product.imageUrl} width={520} height={480} alt=""/>
   </ImageContainer>
   <ProductDetails>
-    <h1>Camiseta X</h1>
-    <span>R$ +8000,00</span>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed illo reiciendis quo dolorem cupiditate aperiam vel? In ut laboriosam, vitae, aliquam iusto quaerat omnis ab nihil sint eius ullam rerum!</p>
+    <h1>{product.name}</h1>
+    <span>{product.price}</span>
+    <p>{product.description}</p>
     <button>Comprar agora</button>
   </ProductDetails>
  </ProductContainer>
   )
+}
+export const getStaticProps: GetStaticProps<any,{id: string}> = async( {params}) => {
+  const productId = params.id;
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+  const price = product.default_price as Stripe.Price | null;
+  return {
+    props: {
+     product: {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price && price.unit_amount
+        ? new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(price.unit_amount / 100)
+        : null,
+        description:product.description,
+     }
+    },
+    revalidate: 60 * 60 * 1
+  };
 }
